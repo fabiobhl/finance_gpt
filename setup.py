82ae93687ce1d017f73b0322e545972bc28425e5
Fabio <fabuehle@spacehopper.ethz.ch>
2023-12-27 11:31:07 +0100

    setup package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='finance-gpt',
+    version='1.0',
+    description='GPT sentiment analysis for finance',
+    author='Fabio Buehler',
+    author_email="fabiobuehler10@gmail.com",
+    packages=['src/finance-gpt'],
+)
